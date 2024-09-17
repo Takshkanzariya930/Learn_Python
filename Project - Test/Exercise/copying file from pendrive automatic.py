412cbc76af7d59de313c48ea4ff1460deca1357f
Takshkanzariya930 <takshkanzariya930@gmail.com>
2024-09-17 22:14:53 +0530

    Removed notification system

--- Project - Test/Exercise/copying file from pendrive automatic.py
+++ Project - Test/Exercise/copying file from pendrive automatic.py
@@ -2,8 +2,6 @@ import win32api
 import os
 import shutil as sh
 from time import sleep
-from win10toast import ToastNotifier
-toaster = ToastNotifier()
 
 check_P = win32api.GetLogicalDriveStrings().split('\000')[:-1]
 print(check_P)
@@ -21,8 +19,6 @@ while True:
 
     sleep(30)
 
-toaster.show_toast("ALERT!!", "CODE RUNNING...", duration=20)
-
 print(drive)
 
 target_dir = f"{check_P[0]}New_Files\\File198"
@@ -34,5 +30,3 @@ if os.path.exists(target_dir):
 
 print("COPYING....")
 sh.copytree(drive[0],target_dir)
-
-toaster.show_toast("ALERT!!", "COMPLETED.", duration=20)
